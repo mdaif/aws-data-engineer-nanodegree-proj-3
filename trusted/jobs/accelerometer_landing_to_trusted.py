120db56ff61b3847b802dd3940237ca8d92be236
mohamed.daif <daif.control@gmail.com>
2023-11-12 19:35:00 +0100

    Add curated zone proofs

--- trusted/jobs/accelerometer_landing_to_trusted.py
+++ trusted/jobs/accelerometer_landing_to_trusted.py
@@ -4,15 +4,8 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from awsglue import DynamicFrame
-
-
-def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
-    for alias, frame in mapping.items():
-        frame.toDF().createOrReplaceTempView(alias)
-    result = spark.sql(query)
-    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
-
+from awsglue.dynamicframe import DynamicFrame
+from pyspark.sql import functions as SqlFuncs
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
@@ -21,51 +14,55 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# Script generated for node Customer Trusted
-CustomerTrusted_node1699385751108 = glueContext.create_dynamic_frame.from_catalog(
-    database="nanodegree",
-    table_name="customer_trusted",
-    transformation_ctx="CustomerTrusted_node1699385751108",
-)
-
 # Script generated for node Accelerometer Landing
-AccelerometerLanding_node1699385750882 = glueContext.create_dynamic_frame.from_catalog(
+AccelerometerLanding_node1699793285313 = glueContext.create_dynamic_frame.from_catalog(
     database="nanodegree",
     table_name="accelerometer_landing",
-    transformation_ctx="AccelerometerLanding_node1699385750882",
+    transformation_ctx="AccelerometerLanding_node1699793285313",
 )
 
-# Script generated for node SQL Query
-SqlQuery0 = """
-SELECT x, y, z, CAST(timestamp AS bigint), user 
-FROM accelerometer_landing
-JOIN customer_trusted
-    ON customer_trusted.email = accelerometer_landing.user
-GROUP BY 1, 2, 3, 4, 5
-;
-"""
-SQLQuery_node1699796794389 = sparkSqlQuery(
+# Script generated for node Customer Trusted
+CustomerTrusted_node1699793311270 = glueContext.create_dynamic_frame.from_catalog(
+    database="nanodegree",
+    table_name="customer_trusted",
+    transformation_ctx="CustomerTrusted_node1699793311270",
+)
+
+# Script generated for node Join
+Join_node1699793371733 = Join.apply(
+    frame1=CustomerTrusted_node1699793311270,
+    frame2=AccelerometerLanding_node1699793285313,
+    keys1=["email"],
+    keys2=["user"],
+    transformation_ctx="Join_node1699793371733",
+)
+
+# Script generated for node Drop Fields
+DropFields_node1699793760590 = DropFields.apply(
+    frame=Join_node1699793371733,
+    paths=["z", "y", "x", "user", "timestamp"],
+    transformation_ctx="DropFields_node1699793760590",
+)
+
+# Script generated for node Drop Duplicates
+DropDuplicates_node1699801587508 = DynamicFrame.fromDF(
+    DropFields_node1699793760590.toDF().dropDuplicates(),
     glueContext,
-    query=SqlQuery0,
-    mapping={
-        "customer_trusted": CustomerTrusted_node1699385751108,
-        "accelerometer_landing": AccelerometerLanding_node1699385750882,
-    },
-    transformation_ctx="SQLQuery_node1699796794389",
+    "DropDuplicates_node1699801587508",
 )
 
 # Script generated for node Amazon S3
-AmazonS3_node1699386006259 = glueContext.getSink(
-    path="s3://daifs-nanodegree-bucket/accelerometer/trusted/",
+AmazonS3_node1699793848209 = glueContext.getSink(
+    path="s3://daifs-nanodegree-bucket/customer/curated/",
     connection_type="s3",
     updateBehavior="UPDATE_IN_DATABASE",
     partitionKeys=[],
     enableUpdateCatalog=True,
-    transformation_ctx="AmazonS3_node1699386006259",
+    transformation_ctx="AmazonS3_node1699793848209",
 )
-AmazonS3_node1699386006259.setCatalogInfo(
-    catalogDatabase="nanodegree", catalogTableName="accelerometer_trusted"
+AmazonS3_node1699793848209.setCatalogInfo(
+    catalogDatabase="nanodegree", catalogTableName="customer_curated"
 )
-AmazonS3_node1699386006259.setFormat("json")
-AmazonS3_node1699386006259.writeFrame(SQLQuery_node1699796794389)
+AmazonS3_node1699793848209.setFormat("json")
+AmazonS3_node1699793848209.writeFrame(DropDuplicates_node1699801587508)
 job.commit()
